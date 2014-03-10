2fbfa83375151ee8f82ff33453b5e7cdf99202f8
marthaberner <martha@marthaberner.com>
2014-03-10 17:50:23 -0600

    Multiples of 15 working. still using if statements

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -11,12 +11,16 @@ def arrayify
   i = 0
 
   while (i < numbers.length)
+    if (numbers[i] % 5 == 0) && (numbers[i] % 3 == 0)
+      numbers[i] = "FizzBuzz"
+    end
     if (numbers[i] % 3 == 0)
       numbers[i] = "Fizz"
     end
     if (numbers[i] % 5 == 0)
       numbers[i] = "Buzz"
     end
+
 i += 1
   end
   numbers
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -12,9 +12,16 @@ describe "FizzBuzz" do
   end
 
   it "Replaces multiples of 3 with Fizz and multiples of 5 with Buzz" do
-  fizzbuzz = FizzBuzz.new(15)
+  fizzbuzz = FizzBuzz.new(14)
   expected  = [ 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,]
   actual = fizzbuzz.arrayify
   end
+it "Now, also replaces multiples of 3 and 5 with FizzBuzz" do
+  fizzbuzz = FizzBuzz.new(15)
+
+  expected  = [ 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+  actual = fizzbuzz.arrayify
   end
+end
+
 
