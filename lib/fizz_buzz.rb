80de15f1da0f05c64c1ef92e7b71405ffaab1a79
marthaberner <martha@marthaberner.com>
2014-03-10 17:41:48 -0600

    Now, also writes Buzz for multiples of 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -14,6 +14,9 @@ def arrayify
     if (numbers[i] % 3 == 0)
       numbers[i] = "Fizz"
     end
+    if (numbers[i] % 5 == 0)
+      numbers[i] = "Buzz"
+    end
 i += 1
   end
   numbers
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -11,9 +11,10 @@ describe "FizzBuzz" do
 
   end
 
-  it "Replaces multiples of 3 with Fizz and multiples of 5 with Buzz"
+  it "Replaces multiples of 3 with Fizz and multiples of 5 with Buzz" do
   fizzbuzz = FizzBuzz.new(15)
-  eexpected  = [ 1, 2, "Fizz", 4, 5, "Fizz", 7, 8, "Fizz", 10,]
+  expected  = [ 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,]
 
   end
+  end 
 
