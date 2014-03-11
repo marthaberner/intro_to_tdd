ab212a995982222c56f9b0244bbdbf35a46a5c7f
marthaberner <martha@marthaberner.com>
2014-03-10 21:18:25 -0600

    Prints the that it is a leap year, but without 'actual' in my test.

---
+++ lib/leap_year.rb
@@ -0,0 +1,20 @@
+class Year
+  def initialize(leap)
+  @leap = leap
+end
+  def yes?
+    frog = (@leap % 4) == 0
+
+    i = 0
+
+    while (i >= 0)
+      if frog == true
+        frog[i] = "It's a leap year!"
+      end
+
+      i -= 1
+    end
+    frog
+
+  end
+  end
---
+++ spec/leap_year_spec.rb
@@ -0,0 +1,13 @@
+require "rspec/core"
+
+require "leap_year"
+
+describe "LeapYear"  do
+  it "Uses a method yes? to check if the year is a leap year"do
+  the_year = Year.new(1996)
+
+  expected = "It's a leap year!"
+  
+    end
+
+end
