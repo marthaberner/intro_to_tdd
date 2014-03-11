4e06d99d6edfec0ef8848c4f30ecb7e7f8d4de09
marthaberner <martha@marthaberner.com>
2014-03-11 15:51:08 -0600

    Method returns 0 if the string passed is empty

---
+++ lib/string_calculator.rb
@@ -0,0 +1,7 @@
+class String_calc
+def add(num)
+  if ""
+    return 0
+  end
+end
+end
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,10 @@
+require "rspec/core"
+
+require "string_calculator"
+
+describe "StringCalculator" do
+  it "Returns 0 if the string passed is empty." do
+    num = String_calc.new()
+
+  end
+  end
