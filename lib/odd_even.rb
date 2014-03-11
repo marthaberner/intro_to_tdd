059140672b3b297d122ca4f9a08352857817f2d4
marthaberner <martha@marthaberner.com>
2014-03-10 19:00:06 -0600

    Code substituting even for even numbers.

--- lib/odd_even.rb
+++ lib/odd_even.rb
@@ -5,7 +5,7 @@ class Numbers
 
   def run
     duck = 1.upto(@the_number).to_a
-  end
+
 
   i = 0
 
@@ -14,6 +14,7 @@ class Numbers
       duck[i] = "Even"
     end
     i += 1
-    end
-duck
-end
+  end
+  duck
+  end
+  end 
