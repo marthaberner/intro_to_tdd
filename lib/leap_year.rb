c6287f545a787318d0a7e73d6fd60620b9d9bc3a
marthaberner <martha@marthaberner.com>
2014-03-11 15:15:36 -0600

    Removed initialized method from the Year class and changed @year from an instance variable to a regular variable

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,12 +1,9 @@
 class Year
-  def initialize(leap)
-  @leap = leap
-end
-  def yes?
-    if(@leap % 400 == 0) && (@leap % 100 == 0)
+  def yes?(leap)
+    if(leap % 400 == 0) && (leap % 100 == 0)
       return "It's a leap year!"
     else
-      "This is not a leap year."
+     return "This is not a leap year."
     end
   end
   end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -4,10 +4,10 @@ require "leap_year"
 
 describe "LeapYear"  do
   it "Uses a method yes? to check if the year is divisible by 400 and 100" do
-  the_year = Year.new(2000)
+  the_year = Year.new()
 
   expected = "It's a leap year!"
-  actual = the_year.yes?
+  actual = the_year.yes?(2000)
 
   expect(actual).to eq(expected)
 
