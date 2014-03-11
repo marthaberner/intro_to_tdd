5b94341f70fef08f28e64676d3c36e6c26af4fbc
marthaberner <martha@marthaberner.com>
2014-03-10 18:58:28 -0600

    Code not returning Even.  still troubleshooting.

--- lib/odd_even.rb
+++ lib/odd_even.rb
@@ -4,8 +4,16 @@ class Numbers
   end
 
   def run
-    my_array = 1.upto(@the_number).to_a
+    duck = 1.upto(@the_number).to_a
+  end
+
+  i = 0
+
+  while (i < duck.length)
+    if (duck[i] % 2 == 0)
+      duck[i] = "Even"
     end
-
-
+    i += 1
+    end
+duck
 end
--- spec/odd_even_spec.rb
+++ spec/odd_even_spec.rb
@@ -9,4 +9,10 @@ describe "OddEven" do
   expected = [1, 2, 3, 4, 5,]
   actual = chosen.run
   end
+  it "Prints Even if a number in the array is even" do
+  chosen = Numbers.new(6)
+
+  expected = [1, "Even", 3, "Even", 5, "Even",]
+  actual = chosen.run
+    end
 end
