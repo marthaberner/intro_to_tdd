2369d8944eedc3ae28e577d9140ae2295756658f
marthaberner <martha@marthaberner.com>
2014-03-11 09:31:37 -0600

    Preparing to push my work, i forgot to do it yesterday

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -4,10 +4,10 @@ require "leap_year"
 
 describe "LeapYear"  do
   it "Uses a method yes? to check if the year is a leap year"do
-  the_year = Year.new(1996)
+  the_year = Year.new(2000)
 
   expected = "It's a leap year!"
-  
+  #actual = the_year.yes? this doesn't work
     end
 
 end
