1ddf61dd1559bf7e0cc89d89e247e893e3bc7854
marthaberner <martha@marthaberner.com>
2014-03-12 10:14:53 -0600

    The method handles a single number input.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,9 @@
-class String_calc
-def add(num)
-  if num == " "
+class StringCalc
+def add(string)
+  if string == ""
     return 0
   else
-   my_array = (num).split(',').map(&:to_i)
+   my_array = (string).split(',').map(&:to_i)
   return my_array.each do |x|
      my_array.inject(0, &:+)
   end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,19 +4,19 @@ require "string_calculator"
 
 describe "StringCalculator" do
   it "Returns 0 if the string passed is empty." do
-    calc = String_calc.new()
+    calc = StringCalc.new()
 
     expected = 0
-    actual = calc.add(" ")
+    actual = calc.add("")
 
     expect(actual).to eq(expected)
 
   end
   it "If the string is not empty, the method returns the sum of the numbers in the string." do
-    calc = String_calc.new()
+    calc = StringCalc.new()
 
-    expected = [6]
-    actual = calc.add("1, 2, 3")
+    expected = [1]
+    actual = calc.add("1")
 
     expect(actual).to eq(expected)
   end
