74f0fa9586baf55fc1438131d7617e9cc7ca575e
Martha Berner <m.berner@awesometown.local>
2014-03-19 16:03:20 -0600

    Multiples of 3 are Fizz
    Signed-off-by: Bebe Peng <b.peng@awesometown.local>

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,3 @@
-
 class FizzBuzz
   def initialize(high_limit)
     @high_limit = high_limit
@@ -6,5 +5,15 @@ class FizzBuzz
 
   def arrayify
     my_array = 1.upto(@high_limit).to_a
+    i = 0
+
+    while i < my_array.length
+      if my_array[i] % 3 == 0
+        my_array[i] = "Fizz"
+      end
+      i += 1
+    end
+    my_array
   end
+
 end
