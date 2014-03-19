61750d8f2dedd2af0db8461cf8f2b30ef363ba70
Martha Berner <m.berner@awesometown.local>
2014-03-19 15:41:16 -0600

    Refactored test
    Signed-off-by: Bebe Peng <b.peng@awesometown.local>

--- spec/key_value_store_spec.rb
+++ spec/key_value_store_spec.rb
@@ -3,45 +3,36 @@ require "rspec/core"
 require "key_value_store"
 
 describe KeyValueStore do
+  before do
+    @key_value_store = KeyValueStore.new
+    @key_value_store.add("foo", 1000)
+  end
+
   it "allows me to add a key and value" do
-    key_value_store = KeyValueStore.new
-
-    key_value_store.add("foo", 1000)
-
-    expect(key_value_store.get("foo")).to eq 1000
+    expect(@key_value_store.get("foo")).to eq 1000
   end
 
   it "Allows for deletion of a single key" do
+    @key_value_store.delete('foo')
 
-    key_value_store = KeyValueStore.new
-    key_value_store.add("foo", 1000)
-
-    key_value_store.delete('foo')
-
-    expect(key_value_store.get('foo')).to eq nil
+    expect(@key_value_store.get('foo')).to eq nil
   end
 
   it "Prints a list of keys" do
-    key_value_store = KeyValueStore.new
-    key_value_store.add("foo", 1000)
 
-    expect(key_value_store.key_list).to eq ["foo"]
+    expect(@key_value_store.key_list).to eq ["foo"]
   end
 
   it "prints the list of many keys" do
-    key_value_store = KeyValueStore.new
-    key_value_store.add("foo", 1000)
-    key_value_store.add("foo2", 1002)
+   @key_value_store.add("foo2", 1002)
 
-    expect(key_value_store.key_list).to eq ["foo","foo2"]
+    expect(@key_value_store.key_list).to eq ["foo","foo2"]
   end
 
   it "deletes all the key, value pairs from the store" do
-    key_value_store = KeyValueStore.new
-    key_value_store.add("foo", 1000)
-    key_value_store.add("foo2", 1002)
+    @key_value_store.add("foo2", 1002)
 
-    expect(key_value_store.clear_store).to eq ({})
+    expect(@key_value_store.clear_store).to eq ({})
 
   end
 end
