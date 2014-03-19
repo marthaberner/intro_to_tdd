ce8489b845d70168493423781cdbaa1974b60fc6
Martha Berner <m.berner@awesometown.local>
2014-03-19 16:05:39 -0600

    Turns multiples of 5 to Buzz
    Signed-off-by: Bebe Peng <b.peng@awesometown.local>

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,18 +5,18 @@ require "fizz_buzz"
 describe "FizzBuzz" do
 
   it "prints an array of 5" do
-    fizz = FizzBuzz.new(5)
-    expected = [1,2,3,4,5]
+    fizz = FizzBuzz.new(2)
+    expected = [1,2]
     actual = fizz.arrayify
 
     expect(actual).to eq expected
   end
 
   it "Multiples of 3 are Fizz" do
-    pending
-  fizzbuzz = FizzBuzz.new(10)
 
-  expected  = [ 1, 2, "Fizz", 4, 5, "Fizz", 7, 8, "Fizz", 10,]
+  fizzbuzz = FizzBuzz.new(4)
+
+  expected  = [ 1, 2, "Fizz", 4]
   actual = fizzbuzz.arrayify
 
     expect(actual).to eq expected
@@ -24,7 +24,6 @@ describe "FizzBuzz" do
   end
 
   it "Replaces multiples of 3 with Fizz and multiples of 5 with Buzz" do
-    pending
   fizzbuzz = FizzBuzz.new(14)
   expected  = [ 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,]
   actual = fizzbuzz.arrayify
