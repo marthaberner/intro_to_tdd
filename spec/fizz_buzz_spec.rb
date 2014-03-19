045ebe9489826f8163195039dc5fdd579a2438ef
Martha Berner <m.berner@awesometown.local>
2014-03-19 16:26:29 -0600

    Refactor while loop using map
    Signed-off-by: Bebe Peng <b.peng@awesometown.local>

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -6,7 +6,7 @@ describe "FizzBuzz" do
 
   it "prints an array of 5" do
     fizz = FizzBuzz.new(2)
-    expected = [1,2]
+    expected = [1, 2]
     actual = fizz.arrayify
 
     expect(actual).to eq expected
@@ -14,30 +14,30 @@ describe "FizzBuzz" do
 
   it "Multiples of 3 are Fizz" do
 
-  fizzbuzz = FizzBuzz.new(4)
+    fizzbuzz = FizzBuzz.new(4)
 
-  expected  = [ 1, 2, "Fizz", 4]
-  actual = fizzbuzz.arrayify
+    expected = [1, 2, "Fizz", 4]
+    actual = fizzbuzz.arrayify
 
     expect(actual).to eq expected
 
   end
 
   it "Replaces multiples of 3 with Fizz and multiples of 5 with Buzz" do
-  fizzbuzz = FizzBuzz.new(14)
-  expected  = [ 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,]
-  actual = fizzbuzz.arrayify
+    fizzbuzz = FizzBuzz.new(14)
+    expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,]
+    actual = fizzbuzz.arrayify
 
-  expect(actual).to eq expected
-
+    expect(actual).to eq expected
   end
-it "Now, also replaces multiples of 3 and 5 with FizzBuzz" do
-  fizzbuzz = FizzBuzz.new(15)
 
-  expected  = [ 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
-  actual = fizzbuzz.arrayify
+  it "Now, also replaces multiples of 3 and 5 with FizzBuzz" do
+    fizzbuzz = FizzBuzz.new(15)
 
-  expect(actual).to eq expected
+    expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+    actual = fizzbuzz.arrayify
+
+    expect(actual).to eq expected
   end
 end
 
